04fcc1a660ac6046f95d1b38f4d0e15f2378afcd
shahdqalalweh <shahdqalalweh2003@gmail.com>
2025-06-21 03:53:49 +0300

    Wrapper Classes

---
+++ src/Wrapper/WrapperClasses.java
@@ -0,0 +1,15 @@
+package Wrapper;
+import java.util.*;
+
+public class WrapperClasses {
+    public static void main(String[] args){
+        Integer i =  Integer.valueOf(5); // No New At declaration
+        Double d = Double.valueOf("3.55555");
+        System.out.println(d);
+        System.out.println(i);
+        System.out.println(i.MAX_VALUE);
+        System.out.println(i.MIN_VALUE);
+
+
+    }
+}
